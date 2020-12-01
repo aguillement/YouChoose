ca2c8ae9a9d61b53de8174233b816d69092d143c
Adrien Guillement <adrien.guillement@gmail.com>
2020-12-01 20:09:20 +0100

    Add game command

--- main.py
+++ main.py
@@ -1,12 +1,40 @@
+from discord.ext import commands
+
+
 import discord
 import os
+import random
+import requests
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
+bot = commands.Bot(command_prefix='!')
 
-    async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
+"""
+Get Bot commands info.
+"""
+@bot.command()
+async def info(ctx):
+    await ctx.send("for choosing **game** : !game *game1, game2, game3*")
+    await ctx.send("for choosing **another** : !choose *word1, word2, word3*")
 
-client = MyClient()
-client.run(os.getenv("DISCORD_TOKEN"))
+"""
+Choose a game.
+"""
+@bot.command()
+async def game(ctx, *args):
+    games = ' '.join(args).split(',')
+    #await ctx.send("{} possibilités: {}".format(len(games), ', '.join(games)))
+    #await ctx.send(random.choice(games))
+
+    game = random.choice(games)
+    # TODO CALL API LIKE : get_game_picture(game)
+
+
+"""
+Choose a random.
+"""
+@bot.command()
+async def choose(ctx, *args):
+    choice = ' '.join(args).split(',')
+    await ctx.send(random.choice(choice))
+
+bot.run(os.getenv("DISCORD_TOKEN"))
