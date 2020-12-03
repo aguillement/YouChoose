d6daabcf67735df21649035a321212b688e506b1
adrien.guillement <adrien.guillement@gmail.com>
2020-12-03 21:06:06 +0100

    Change comment

--- request.py
+++ request.py
@@ -19,7 +19,7 @@ def get_game_picture(game_name):
     response = requests.post('https://api.igdb.com/v4/games', headers=headers, data=request_query)
     data = json.loads(response.content)
     
-    # replace t_thumb with t_1080p
+    # Replace t_thumb with t_1080p, to get bigger image
     image_url = data[0]["screenshots"][0]["url"].replace("t_thumb", "t_1080p")
 
     return "https:{}".format(image_url)
